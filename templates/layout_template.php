ef7ecf3f45d54994a4180f471a5acfeac52039ef
Vo Pham Khanh Truong <voxtruong93@gmail.com>
2024-12-19 08:36:02 +0700

    commit page home

---
+++ templates/layout_template.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+	ustora by freshdesignweb.com
+	Twitter: https://twitter.com/freshdesignweb
+	URL: https://www.freshdesignweb.com/ustora/
+-->
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ustora Demo</title>
+    
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../public/userslte/css/bootstrap.min.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../public/userslte/css/font-awesome.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../public/userslte/css/owl.carousel.css">
+    <link rel="stylesheet" href="../public/userslte/css/style.css">
+    <link rel="stylesheet" href="../public/userslte/css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class=" ">
+        <?= $content ?>
+    </div>
+    
+    <!-- Latest jQuery form server -->
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
+    <!-- jQuery sticky menu -->
+    <script src="../public/userslte/js/owl.carousel.min.js"></script>
+    <script src="../public/userslte/js/jquery.sticky.js"></script>
+    
+    <!-- jQuery easing -->
+    <script src="../public/userslte/js/jquery.easing.1.3.min.js"></script>
+    
+    <!-- Main Script -->
+    <script src="../public/userslte/js/main.js"></script>
+    
+    <!-- Slider -->
+    <script type="text/javascript" src="../public/userslte/js/bxslider.min.js"></script>
+	<script type="text/javascript" src="../public/userslte/js/script.slider.js"></script>
+  </body>
+</html>
